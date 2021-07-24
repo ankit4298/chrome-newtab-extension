04253544c4d29955f5838def1390f4d03be58101
ankit4298 <ankit4298@gmail.com>
2021-07-24 19:48:58 +0530

    optimized time display and async functions

--- script.js
+++ script.js
@@ -18,12 +18,17 @@ function currentTimeAndDate() {
 
 //Fetch image from lorem picsum and set it as background
 //Todo: Set color as per background
-async function setBackgroundImage() {
+function setBackgroundImage() {
     const screenWidth = window.innerWidth;
     const screenHeight = window.innerHeight;
     const url = `https://picsum.photos/${screenWidth}/${screenHeight}?blur=5`;
-    const image = await fetch(url);
-    document.querySelector('.container').style = "background-size: cover;background: url(" + image.url + ") center top";
+    const image = fetch(url);
+    image.then(e=>{
+        document.querySelector('.container').style = "background-size: cover;background: url(" + e.url + ") center top";
+    }).catch(ex=>{
+        alert('Something went wrong fetching background image!!!');
+        console.log(ex);
+    })
 }
 
 
@@ -57,15 +62,13 @@ function setTime() {
     const fullTime = currentTimeAndDate();
 
     timeText.innerHTML = `${fullTime.hour}: ${fullTime.minutes}: ${fullTime.seconds}`;
-
 }
 
 
 
 
 
-function setName(fullTime) {
-
+function setName() {
 
     if (!localStorage.getItem('name') || localStorage.getItem('name') === "") {
         const inputName = document.querySelector('.popup__input').value;
@@ -86,20 +89,23 @@ function setName(fullTime) {
         const inputName = document.querySelector('.popup__input').value;
         document.querySelector('.greeting__name').innerHTML = localStorageName === "" ? inputName : localStorageName;
     }
-    setGreeting(fullTime);
-
 
 }
 
 
-async function getRandomQuote() {
+function getRandomQuote() {
 
-    const response = await fetch('https://api.quotable.io/random?maxLength=200&minLength=100').then(res => res.json());
+    const response = fetch('https://api.quotable.io/random?maxLength=200&minLength=100').then(res => res.json());
 
-    const quoteContentDOM = document.querySelector('.quote__content');
-    const quoteAuthorDOM = document.querySelector('.quote__author')
-    quoteContentDOM.innerHTML = response.content;
-    quoteAuthorDOM.innerHTML = `- ${response.author}`;
+    response.then(e=>{
+        const quoteContentDOM = document.querySelector('.quote__content');
+        const quoteAuthorDOM = document.querySelector('.quote__author')
+        quoteContentDOM.innerHTML = e.content;
+        quoteAuthorDOM.innerHTML = `- ${e.author}`;
+    }).catch(ex=>{
+        alert('Something went wrong fetching quote!!!');
+        console.log(ex);
+    })
 
 }
 
@@ -131,17 +137,27 @@ function recordLastVisited(){
 }
 
 
+function onStartup(fullTime){
+    // run once
+    setName();
+    setGreeting(fullTime);
+    setTime();
+    setDate();
+
+    document.querySelector('.popup__button').addEventListener('click', setName.bind(this, fullTime));
+
+    // fetch 3rd party
+    setBackgroundImage();
+    getRandomQuote();
+}
+
 
 var fullTime = currentTimeAndDate();
-setName(fullTime);
-document.querySelector('.popup__button').addEventListener('click', setName.bind(this, fullTime));
-setGreeting(fullTime);
-setBackgroundImage();
-getRandomQuote();
+onStartup(fullTime);
+
+// run continuously
 setInterval(setTime, 1000);
 setInterval(setDate, 1000);
-
-
 
 
 
@@ -205,9 +221,3 @@ setInterval(setDate, 1000);
 (function () {
     if (localStorage.length == 0) localStorage.setItem('showNamePopup', true);
 })();
-
-// localStorage.clear();
-
-
-
-console.table(localStorage);
